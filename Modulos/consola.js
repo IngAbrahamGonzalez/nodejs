69ad037dc9541c1267912ebf722b8cc1c6422099
Ing. Abraham Gonzalez <abrahamgonz93@gmail.com>
2023-09-29 13:35:25 -0600

    se agrego mas contenido de nodejs

--- Modulos/consola.js
+++ Modulos/consola.js
@@ -1,1 +1,20 @@
-console.log('algo')
+// var tabla = [
+//     {
+//         a : 1, 
+//         b : 'Z'
+//     },
+//     {
+//         a : 2,
+//         b : 'Y'
+//     }
+// ]
+//console.table(tabla)
+
+
+console.group('Conversacion');
+console.log('Hola');
+console.log('Blah blah blah');
+console.log('Adios');
+console.groupEnd('Conversacion');
+
+console.log('otra cosa de otra funcion');
