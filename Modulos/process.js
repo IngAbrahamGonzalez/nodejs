42f8b05497508a704033b51803de0393b8aa509d
Ing. Abraham Gonzalez <abrahamgonz93@gmail.com>
2023-10-03 13:17:29 -0600

    se agrego contenido

--- Modulos/process.js
+++ Modulos/process.js
@@ -1,5 +1,9 @@
 // const process = require('process');
 
+process.on('beforeExit', () => {
+    console.log('El proceso va a terminar');
+})
+
 process.on('exit', () => {
     console.log('Ale, el proceso se acabó');
 })
